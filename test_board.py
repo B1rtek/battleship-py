26acabf921edab76c0fec0550406ba3608f978dd
B1rtek <b1rtek@fakemail.com>
2022-01-17 01:31:23 +0100

    Completed test_board.py

--- test_board.py
+++ test_board.py
@@ -195,15 +195,18 @@ def test_gameboard_create():
             ship_segment_poss.append(segment.position())
     board.place_fleet(fleet)
     gboard = GameBoard(board)
-    visible_board = gboard.get_display_board(display_as_enemy=True)
+    board_seen_by_enemy = gboard.get_display_board(display_as_enemy=True)
     for x, y in get_all_fields_coordinates():
-        assert visible_board.get_field_status(x, y) == FieldStatus.NOTHING
-    data_board = gboard.get_display_board()
+        assert board_seen_by_enemy. \
+                   get_field_status(x, y) == FieldStatus.NOTHING
+    board_seen_by_player = gboard.get_display_board()
     for x, y in get_all_fields_coordinates():
         if (x, y) in ship_segment_poss:
-            assert data_board.get_field_status(x, y) == FieldStatus.SHIP
+            assert board_seen_by_player. \
+                       get_field_status(x, y) == FieldStatus.SHIP
         else:
-            assert data_board.get_field_status(x, y) == FieldStatus.NOTHING
+            assert board_seen_by_player. \
+                       get_field_status(x, y) == FieldStatus.NOTHING
 
 
 def test_gameboard_discover_field_miss():
@@ -211,10 +214,10 @@ def test_gameboard_discover_field_miss():
     gboard = GameBoard(board)
     hit = gboard.discover_field('b', 1)
     assert not hit
-    data_board = gboard.get_display_board()
-    assert data_board.get_field_status('b', 1) == FieldStatus.MISS
-    visible_board = gboard.get_display_board(display_as_enemy=True)
-    assert visible_board.get_field_status('b', 1) == FieldStatus.MISS
+    board_seen_by_player = gboard.get_display_board()
+    assert board_seen_by_player.get_field_status('b', 1) == FieldStatus.MISS
+    board_seen_by_enemy = gboard.get_display_board(display_as_enemy=True)
+    assert board_seen_by_enemy.get_field_status('b', 1) == FieldStatus.MISS
 
 
 def test_gameboard_discover_field_hit():
@@ -227,10 +230,10 @@ def test_gameboard_discover_field_hit():
     x, y = guaranteed_hit_pos
     hit = gboard.discover_field(x, y)
     assert hit
-    data_board = gboard.get_display_board()
-    assert data_board.get_field_status(x, y) == FieldStatus.SUNK
-    visible_board = gboard.get_display_board(display_as_enemy=True)
-    assert visible_board.get_field_status(x, y) == FieldStatus.SHIP
+    board_seen_by_player = gboard.get_display_board()
+    assert board_seen_by_player.get_field_status(x, y) == FieldStatus.SUNK
+    board_seen_by_enemy = gboard.get_display_board(display_as_enemy=True)
+    assert board_seen_by_enemy.get_field_status(x, y) == FieldStatus.SHIP
 
 
 def test_gameboard_discover_field_sunk():
@@ -243,10 +246,22 @@ def test_gameboard_discover_field_sunk():
     x, y = guaranteed_sink_pos
     hit = gboard.discover_field(x, y)
     assert hit
-    data_board = gboard.get_display_board()
-    assert data_board.get_field_status(x, y) == FieldStatus.SUNK
-    visible_board = gboard.get_display_board(display_as_enemy=True)
-    assert visible_board.get_field_status(x, y) == FieldStatus.SUNK
+    board_seen_by_player = gboard.get_display_board()
+    assert board_seen_by_player.get_field_status(x, y) == FieldStatus.SUNK
+    board_seen_by_enemy = gboard.get_display_board(display_as_enemy=True)
+    assert board_seen_by_enemy.get_field_status(x, y) == FieldStatus.SHIP
+    # This is intended behaviour. Only after confirming that the ship was sunk
+    # the sink_ship() method is called and the ship gets marked as sunk for
+    # the enemy, this suggests that the hit ship isn't sunk yet and the enemy
+    # should search for the other parts of it
+    sunk = fleet.hit(x, y)
+    assert sunk
+    ship_to_sink = fleet.find_ship(x, y)
+    gboard.sink_ship(ship_to_sink)
+    board_seen_by_player = gboard.get_display_board()
+    assert board_seen_by_player.get_field_status(x, y) == FieldStatus.SUNK
+    board_seen_by_enemy = gboard.get_display_board(display_as_enemy=True)
+    assert board_seen_by_enemy.get_field_status(x, y) == FieldStatus.SUNK
 
 
 def test_gameboard_mark_as_empty():
@@ -254,23 +269,193 @@ def test_gameboard_mark_as_empty():
     gboard = GameBoard(board)
     marked = gboard.mark_as_empty('a', 1)
     assert marked
-    data_board = gboard.get_display_board()
-    assert data_board.get_field_status('a', 1) == FieldStatus.NOTHING
-    visible_board = gboard.get_display_board(display_as_enemy=True)
-    assert visible_board.get_field_status('a', 1) == FieldStatus.MISS
+    board_seen_by_player = gboard.get_display_board()
+    assert board_seen_by_player.get_field_status('a', 1) == FieldStatus.MISS
+    board_seen_by_enemy = gboard.get_display_board(display_as_enemy=True)
+    assert board_seen_by_enemy.get_field_status('a', 1) == FieldStatus.MISS
+    # Both players can see which fields have been marked by their enemy. It
+    # gives the player the idea how much moves he can survive, and the markers
+    # placed by the player prevent the player from shooting at these fields.
 
 
-def test_gameboard_mark_as_empty_fail():
+def test_gameboard_mark_as_empty_fail_field_marked():
     board = Board()
     gboard = GameBoard(board)
     marked = gboard.mark_as_empty('a', 1)
     assert marked
     marked_again = gboard.mark_as_empty('a', 1)
     assert not marked_again
-    data_board = gboard.get_display_board()
-    assert data_board.get_field_status('a', 1) == FieldStatus.NOTHING
-    visible_board = gboard.get_display_board(display_as_enemy=True)
-    assert visible_board.get_field_status('a', 1) == FieldStatus.MISS
+    board_seen_by_player = gboard.get_display_board()
+    assert board_seen_by_player.get_field_status('a', 1) == FieldStatus.MISS
+    board_seen_by_enemy = gboard.get_display_board(display_as_enemy=True)
+    assert board_seen_by_enemy.get_field_status('a', 1) == FieldStatus.MISS
 
 
+def test_gameboard_mark_as_empty_fail_ship_on_field():
+    board = Board()
+    fleet = Fleet()
+    fleet.create_random()
+    board.place_fleet(fleet)
+    guaranteed_hit_pos = fleet.ships()[0].segments()[0].position()
+    gboard = GameBoard(board)
+    x, y = guaranteed_hit_pos
+    hit = gboard.discover_field(x, y)
+    assert hit
+    marked = gboard.mark_as_empty(x, y)
+    assert not marked
+    board_seen_by_player = gboard.get_display_board()
+    assert board_seen_by_player.get_field_status(x, y) == FieldStatus.SUNK
+    board_seen_by_enemy = gboard.get_display_board(display_as_enemy=True)
+    assert board_seen_by_enemy.get_field_status(x, y) == FieldStatus.SHIP
 
+
+def test_gameboard_unmark_as_empty():
+    board = Board()
+    gboard = GameBoard(board)
+    marked = gboard.mark_as_empty('a', 1)
+    assert marked
+    unmarked = gboard.unmark_as_empty('a', 1)
+    assert unmarked
+    board_seen_by_player = gboard.get_display_board()
+    assert board_seen_by_player.get_field_status('a', 1) == FieldStatus.NOTHING
+    board_seen_by_enemy = gboard.get_display_board(display_as_enemy=True)
+    assert board_seen_by_enemy.get_field_status('a', 1) == FieldStatus.NOTHING
+
+
+def test_gameboard_unmark_as_empty_fail_not_marked():
+    board = Board()
+    gboard = GameBoard(board)
+    unmarked = gboard.unmark_as_empty('a', 1)
+    assert not unmarked
+    board_seen_by_player = gboard.get_display_board()
+    assert board_seen_by_player.get_field_status('a', 1) == FieldStatus.NOTHING
+    board_seen_by_enemy = gboard.get_display_board(display_as_enemy=True)
+    assert board_seen_by_enemy.get_field_status('a', 1) == FieldStatus.NOTHING
+
+
+def test_gameboard_unmark_as_empty_fail_field_not_empty():
+    board = Board()
+    fleet = Fleet()
+    fleet.create_random()
+    board.place_fleet(fleet)
+    guaranteed_hit_pos = fleet.ships()[0].segments()[0].position()
+    gboard = GameBoard(board)
+    x, y = guaranteed_hit_pos
+    hit = gboard.discover_field(x, y)
+    assert hit
+    unmarked = gboard.unmark_as_empty(x, y)
+    assert not unmarked
+    board_seen_by_player = gboard.get_display_board()
+    assert board_seen_by_player.get_field_status(x, y) == FieldStatus.SUNK
+    board_seen_by_enemy = gboard.get_display_board(display_as_enemy=True)
+    assert board_seen_by_enemy.get_field_status(x, y) == FieldStatus.SHIP
+
+
+def test_gameboard_sink_ship():
+    board = Board()
+    fleet = Fleet()
+    fleet.create_random()
+    board.place_fleet(fleet)
+    ship_segments_to_sink = [x.position() for x in fleet.ships()[2].segments()]
+    gboard = GameBoard(board)
+    board_seen_by_player = gboard.get_display_board()
+    board_seen_by_enemy = gboard.get_display_board(display_as_enemy=True)
+    for x, y in ship_segments_to_sink:
+        assert board_seen_by_player. \
+                   get_field_status(x, y) == FieldStatus.SHIP
+        assert board_seen_by_enemy. \
+                   get_field_status(x, y) == FieldStatus.NOTHING
+    for x, y in ship_segments_to_sink:
+        gboard.discover_field(x, y)
+    board_seen_by_player = gboard.get_display_board()
+    board_seen_by_enemy = gboard.get_display_board(display_as_enemy=True)
+    for x, y in ship_segments_to_sink:
+        assert board_seen_by_player. \
+                   get_field_status(x, y) == FieldStatus.SUNK
+        assert board_seen_by_enemy. \
+                   get_field_status(x, y) == FieldStatus.SHIP
+    x, y = ship_segments_to_sink[0]
+    ship_to_sink = fleet.find_ship(x, y)
+    gboard.sink_ship(ship_to_sink)
+    board_seen_by_player = gboard.get_display_board()
+    board_seen_by_enemy = gboard.get_display_board(display_as_enemy=True)
+    for x, y in ship_segments_to_sink:
+        assert board_seen_by_player. \
+                   get_field_status(x, y) == FieldStatus.SUNK
+        assert board_seen_by_enemy. \
+                   get_field_status(x, y) == FieldStatus.SUNK
+
+
+def test_gameboard_field_undiscovered():
+    board = Board()
+    fleet = Fleet()
+    fleet.create_random()
+    board.place_fleet(fleet)
+    gboard = GameBoard(board)
+    assert gboard.field_undiscovered('a', 1)
+
+
+def test_gameboard_field_undiscovered_discovered():
+    board = Board()
+    fleet = Fleet()
+    fleet.create_random()
+    board.place_fleet(fleet)
+    gboard = GameBoard(board)
+    gboard.discover_field('a', 1)
+    assert not gboard.field_undiscovered('a', 1)
+
+
+def test_mark_misses_around(monkeypatch):
+    def rigged_fleet(self):
+        self._ships.clear()
+        ships = []
+        ships.append(Ship(('a', 1), 4, False))
+        ships.append(Ship(('a', 3), 3, False))
+        ships.append(Ship(('a', 5), 3, False))
+        ships.append(Ship(('a', 7), 2, False))
+        ships.append(Ship(('a', 9), 2, False))
+        ships.append(Ship(('f', 5), 2, False))
+        ships.append(Ship(('f', 1), 1, False))
+        ships.append(Ship(('h', 1), 1, False))
+        ships.append(Ship(('j', 1), 1, False))
+        ships.append(Ship(('j', 10), 1, False))
+        self._ships = ships
+
+    monkeypatch.setattr("fleet.Fleet.create_random", rigged_fleet)
+
+    board = Board()
+    fleet = Fleet()
+    fleet.create_random()
+    board.place_fleet(fleet)
+    gboard = GameBoard(board)
+    ship_to_sink = fleet.find_ship('f', 5)
+    segments = [x.position() for x in ship_to_sink.segments()]
+    fields_around = [('e', 4), ('e', 5), ('e', 6), ('f', 4), ('f', 6),
+                     ('g', 4), ('g', 6), ('h', 4), ('h', 5), ('h', 6), ]
+    gboard.sink_ship(ship_to_sink)
+    board_seen_by_player = gboard.get_display_board()
+    board_seen_by_enemy = gboard.get_display_board(display_as_enemy=True)
+    for x, y in segments:
+        assert board_seen_by_player. \
+                   get_field_status(x, y) == FieldStatus.SUNK
+        assert board_seen_by_enemy. \
+                   get_field_status(x, y) == FieldStatus.SUNK
+    for x, y in fields_around:
+        assert board_seen_by_player. \
+                   get_field_status(x, y) == FieldStatus.NOTHING
+        assert board_seen_by_enemy. \
+                   get_field_status(x, y) == FieldStatus.NOTHING
+    gboard.mark_misses_around(ship_to_sink)
+    for x, y in segments:
+        assert board_seen_by_player. \
+                   get_field_status(x, y) == FieldStatus.SUNK
+        assert board_seen_by_enemy. \
+                   get_field_status(x, y) == FieldStatus.SUNK
+    for x, y in fields_around:
+        assert board_seen_by_player. \
+                   get_field_status(x, y) == FieldStatus.NOTHING
+        assert board_seen_by_enemy. \
+                   get_field_status(x, y) == FieldStatus.MISS
+    # This function is used to automatically mark fields around when the ship
+    # sinks, and these markers cannot be seen by the player as they aren't
+    # placed by the enemy, but show up on their board automatically
